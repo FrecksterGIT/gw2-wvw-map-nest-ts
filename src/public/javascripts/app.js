87cedd936c8fbd474ff91caf85e78e02db312e1d
Sven Müller <sven.mueller@breathless-code.de>
2018-04-28 03:37:47 +0200

    feat: show matchups and subscribe to them

--- src/public/javascripts/app.js
+++ src/public/javascripts/app.js
@@ -1,16 +1,22 @@
 // tslint:disable:no-console
 
-const socket = io('/update');
-socket.on('connect', () => {
-  console.log('connected, subscribing for match');
-  socket.emit('subscribe', '2-1');
-});
-socket.on('subscribed', (data) => {
-  console.log('successfully subscribed for', data);
-});
-socket.on('update', (data) => {
-  // console.log(data);
-});
+class Socket {
+  constructor() {
+    this.socket = io('/update');
+    this.socket.on('connect', () => {
+      console.log('connected, subscribing for match');
+      socket.emit('subscribe', '2-1');
+    });
+    this.socket.on('subscribed', (data) => {
+      console.log('successfully subscribed for', data);
+    });
+  }
+  emit(event, data) {
+    this.socket.emit(event, data);
+  }
+}
+
+const socket = new Socket();
 
 document.querySelectorAll('.match').forEach((match) => {
   match.addEventListener('click', (event) => {
