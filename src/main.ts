b70c09f07bce4a70d2578450c871b5b1e79ec0ac
Sven Müller <sven.mueller@breathless-code.de>
2018-04-30 21:48:34 +0200

    feat: minify html in prod

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import {INestApplication} from '@nestjs/common';
 import {NestFactory} from '@nestjs/core';
 import * as express from 'express';
+
+import * as minifyHTML from 'express-minify-html';
 import * as path from 'path';
 import {Config} from './app.config';
 import {ApplicationModule} from './app.module';
@@ -10,6 +12,7 @@ const PORT = process.env.PORT || 3000;
 async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule);
   setupViewEngine(app);
+  setupMinify(app);
   await app.startAllMicroservicesAsync();
   await app.listen(PORT);
 }
@@ -19,6 +22,23 @@ function setupViewEngine(app: INestApplication) {
   app.set('views', Config.viewDirectory);
   app.set('view options', {layout: 'layouts/main'});
   app.set('view engine', 'hbs');
+}
+
+function setupMinify(app: INestApplication) {
+  if (process.env.NODE_ENV !== 'development') {
+    app.use(minifyHTML({
+      exception_url: false,
+      htmlMinifier: {
+        collapseBooleanAttributes: true,
+        collapseWhitespace: true,
+        minifyJS: true,
+        removeAttributeQuotes: true,
+        removeComments: true,
+        removeEmptyAttributes: true
+      },
+      override: true
+    }));
+  }
 }
 
 bootstrap();
