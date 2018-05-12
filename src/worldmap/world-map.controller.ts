e4bd16caea029f9fdaae91e16bfb97ff2b1a3569
Sven Müller <sven.mueller@breathless-code.de>
2018-05-12 22:57:22 +0200

    feat: add version and link to github project

--- src/worldmap/world-map.controller.ts
+++ src/worldmap/world-map.controller.ts
@@ -2,6 +2,8 @@ import {Controller, Get, Param, Render} from '@nestjs/common';
 import {IMatchDisplay} from '../gw2api/interfaces/match-display.interface';
 import {WorldMapService} from './world-map.service';
 
+import pjson = require('pjson');
+
 @Controller()
 export class WorldMapController {
 
@@ -13,11 +15,13 @@ export class WorldMapController {
   public async root(@Param('lang') lang: string = 'en') {
     const matches: IMatchDisplay[] = await this.mapService.getMatchesData(lang);
     const objectives = await this.mapService.getObjectives(lang);
+
     return {
       locale: lang,
       map: objectives,
       matches,
-      title: 'Guild Wars 2 - WvW Map'
+      title: 'Guild Wars 2 - WvW Map',
+      version: pjson.version
     };
   }
 
