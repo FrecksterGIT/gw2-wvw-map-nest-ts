d9594b9dba00f56a346bd4d063e3c3762f771bac
Sven Müller <sven.mueller@breathless-code.de>
2018-06-09 21:23:23 +0200

    feat: add api status (based on last update) to healthcheck info

--- src/healthcheck/healthcheck.module.ts
+++ src/healthcheck/healthcheck.module.ts
@@ -1,10 +1,11 @@
 import {Module} from '@nestjs/common';
+import {Gw2ApiModule} from '../gw2api/gw2-api.module';
 import {UpdateModule} from '../update/update.module';
 import {HealthcheckController} from './healthcheck.controller';
 
 @Module({
   controllers: [HealthcheckController],
-  imports: [UpdateModule]
+  imports: [UpdateModule, Gw2ApiModule]
 })
 export class HealthcheckModule {
 }
