f7d013b1fbe1eeac17a6023fd10d18510719cb42
Sven Müller <sven.mueller@breathless-code.de>
2019-03-20 20:14:13 +0100

    feat: init optimization

--- src/main.ts
+++ src/main.ts
@@ -8,45 +8,49 @@ import {ApplicationModule} from './app.module';
 
 import compression = require('compression');
 import dotenv = require('dotenv-safe');
-import hbs = require('hbs');
+import exphbs = require('express-handlebars');
 import i18n = require('i18n');
 
 dotenv.config();
 const PORT: number = Number(process.env.PORT) || 3000;
 
 async function bootstrap() {
-  const app = await NestFactory.create(ApplicationModule);
-  app.use(compression());
-  setupViewEngine(app);
-  setupI18n();
+  const server = setupViewEngine();
+  const app = await NestFactory.create(ApplicationModule, server);
   setupMinify(app);
   app.disable('x-powered-by');
   await app.startAllMicroservicesAsync();
   await app.listen(PORT);
 }
 
-function setupViewEngine(app) {
-
-  app.use('/static', express.static(
-    path.join(__dirname, (process.env.NODE_ENV === 'development') ? '../dist/public' : './public')));
-  app.set('views', Config.viewDirectory);
-  app.set('view options', {layout: 'layouts/main'});
-  app.set('view engine', 'hbs');
-}
-
-function setupI18n() {
+function setupViewEngine() {
+  const expressInstance = express();
   i18n.configure({
     directory: path.join(__dirname, './public/locales'),
     locales: ['en', 'de', 'es', 'fr'],
     updateFiles: false
   });
-  hbs.registerHelper('t', (key, locale) => {
-    return i18n.__({phrase: key, locale});
+  const hbs = exphbs.create({
+    defaultLayout: 'main',
+    layoutsDir: (process.env.NODE_ENV === 'development') ? 'src/views/layouts' : 'views/layouts',
+    helpers: {
+      t: (key, locale) => i18n.__({phrase: key, locale})
+    }
   });
+
+  expressInstance.engine('handlebars', hbs.engine);
+  expressInstance.set('view engine', 'handlebars');
+  expressInstance.set('views', Config.viewDirectory);
+
+  expressInstance.use('/static', express.static(
+    path.join(__dirname, (process.env.NODE_ENV === 'development') ? '../dist/public' : './public')));
+
+  return expressInstance;
 }
 
 function setupMinify(app: INestApplication) {
   if (process.env.NODE_ENV !== 'development') {
+    app.use(compression());
     app.use(minifyHTML({
       exception_url: false,
       htmlMinifier: {
