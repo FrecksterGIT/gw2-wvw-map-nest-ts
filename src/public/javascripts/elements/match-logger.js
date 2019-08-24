3c08301d9bdcb271582ca210d37f0529b3694436
Sven Müller <sven.mueller@breathless-code.de>
2019-08-24 17:23:46 +0200

    chore: dependency updates

--- src/public/javascripts/elements/match-logger.js
+++ src/public/javascripts/elements/match-logger.js
@@ -1,7 +1,7 @@
 import log from 'debug';
 import UpdateReceiverElement from './update-receiver-element';
 import Handlebars from 'handlebars';
-import isToday from 'date-fns/is_today';
+import {isToday} from 'date-fns';
 import delegate from 'delegate';
 import MatchLogEntry from '../utils/match-log-entry';
 
@@ -11,112 +11,112 @@ const logger = log('MatchLogger');
 
 let worlds = {};
 const speechMap = {
-  de: 'de-DE',
-  en: 'en-US',
-  es: 'es-ES',
-  fr: 'fr-FR'
+    de: 'de-DE',
+    en: 'en-US',
+    es: 'es-ES',
+    fr: 'fr-FR'
 };
 const MAX_BULK_READ = 5;
 
 export default class MatchLogger extends UpdateReceiverElement {
 
-  constructor() {
-    super();
-    Handlebars.registerHelper('formatTime', dateTime => MatchLogger.formatTime(dateTime));
-  }
-
-  static formatTime(dateTime) {
-    const date = new Date(dateTime);
-    if (!isToday(date)) {
-      return date.toLocaleString();
+    constructor() {
+        super();
+        Handlebars.registerHelper('formatTime', dateTime => MatchLogger.formatTime(dateTime));
     }
-    return date.toLocaleTimeString();
-  }
 
-  connectedCallback() {
-    super.connectedCallback();
-
-    const ownerTemplate = this.querySelector('#log-owner-template').innerHTML;
-    const claimTemplate = this.querySelector('#log-claim-template').innerHTML;
-    this.templateFunctions = {
-      claim: Handlebars.compile(claimTemplate),
-      owner: Handlebars.compile(ownerTemplate)
-    };
-    this.initNewMatch = true;
-    this.initializeHightlighter();
-  }
-
-  initializeHightlighter() {
-    delegate(this, '.log_entry', 'click', (event) => {
-      const objectId = event.delegateTarget.getAttribute('data-changed-objective');
-      const object = document.querySelector('gw2-objective[data-id="' + objectId + '"]');
-      object.highlight();
-    });
-  }
-
-  handleSubscribeUpdate(data) {
-    const matchId = this.getAttribute('data-match-id');
-    if (matchId !== data.id) {
-      this.innerHTML = '';
-      MatchLogEntry.resetLog();
-      worlds = data.payload.main_worlds;
-      this.initNewMatch = true;
+    static formatTime(dateTime) {
+        const date = new Date(dateTime);
+        if (!isToday(date)) {
+            return date.toLocaleString();
+        }
+        return date.toLocaleTimeString();
     }
-  }
 
-  handleObjectiveUpdate(data) {
-    const currentChanges = data.payload
-      .map((objectData) => {
-        const logObject = MatchLogEntry.getLogObject(objectData, worlds);
-        return logObject.getChanges(objectData);
-      })
-      .reduce((changes, entry) => {
-        changes.push(...entry);
-        return changes;
-      }, [])
-      .sort((a, b) => {
-        const aAt = Date.parse(a.at);
-        const bAt = Date.parse(b.at);
-        return aAt < bAt ? -1 : (aAt > bAt ? 1 : 0);
-      });
-    this.logChanges(currentChanges);
-  }
+    connectedCallback() {
+        super.connectedCallback();
 
-  logChanges(changes = []) {
-    changes.forEach((change, index) => {
-      const element = this.logChange(change);
-      if (index >= changes.length - MAX_BULK_READ && this.isSpeechActive(change)) {
-        MatchLogger.readChange(element);
-      }
-    });
-    this.initNewMatch = false;
-  }
+        const ownerTemplate = this.querySelector('#log-owner-template').innerHTML;
+        const claimTemplate = this.querySelector('#log-claim-template').innerHTML;
+        this.templateFunctions = {
+            claim: Handlebars.compile(claimTemplate),
+            owner: Handlebars.compile(ownerTemplate)
+        };
+        this.initNewMatch = true;
+        this.initializeHightlighter();
+    }
 
-  logChange(change) {
-    const content = this.templateFunctions[change.type](change);
-    const template = document.createElement('template');
-    template.innerHTML = content;
-    const element = template.content.firstElementChild;
-    this.insertAdjacentElement('afterbegin', element);
-    return element;
-  }
+    initializeHightlighter() {
+        delegate(this, '.log_entry', 'click', (event) => {
+            const objectId = event.delegateTarget.getAttribute('data-changed-objective');
+            const object = document.querySelector('gw2-objective[data-id="' + objectId + '"]');
+            object.highlight();
+        });
+    }
 
-  static readChange(element) {
-    const logText = element.querySelector('.log_message').textContent;
-    speechSynthesis(logText, speechMap[i18n.getLocale()]);
-  }
+    handleSubscribeUpdate(data) {
+        const matchId = this.getAttribute('data-match-id');
+        if (matchId !== data.id) {
+            this.innerHTML = '';
+            MatchLogEntry.resetLog();
+            worlds = data.payload.main_worlds;
+            this.initNewMatch = true;
+        }
+    }
 
-  get focussedMap() {
-    return this.getAttribute('data-focussed-map') || '';
-  }
+    handleObjectiveUpdate(data) {
+        const currentChanges = data.payload
+            .map((objectData) => {
+                const logObject = MatchLogEntry.getLogObject(objectData, worlds);
+                return logObject.getChanges(objectData);
+            })
+            .reduce((changes, entry) => {
+                changes.push(...entry);
+                return changes;
+            }, [])
+            .sort((a, b) => {
+                const aAt = Date.parse(a.at);
+                const bAt = Date.parse(b.at);
+                return aAt < bAt ? -1 : (aAt > bAt ? 1 : 0);
+            });
+        this.logChanges(currentChanges);
+    }
 
-  isSpeechActive(change) {
-    return (this.focussedMap === '' || change.mapId === this.focussedMap)
-      && MatchLogger.speechSetOn
-      && !this.initNewMatch;
-  }
+    logChanges(changes = []) {
+        changes.forEach((change, index) => {
+            const element = this.logChange(change);
+            if (index >= changes.length - MAX_BULK_READ && this.isSpeechActive(change)) {
+                MatchLogger.readChange(element);
+            }
+        });
+        this.initNewMatch = false;
+    }
 
-  static get speechSetOn() {
-    return !!document.querySelector('.audio.on');
-  }
+    logChange(change) {
+        const content = this.templateFunctions[change.type](change);
+        const template = document.createElement('template');
+        template.innerHTML = content;
+        const element = template.content.firstElementChild;
+        this.insertAdjacentElement('afterbegin', element);
+        return element;
+    }
+
+    static readChange(element) {
+        const logText = element.querySelector('.log_message').textContent;
+        speechSynthesis(logText, speechMap[i18n.getLocale()]);
+    }
+
+    get focussedMap() {
+        return this.getAttribute('data-focussed-map') || '';
+    }
+
+    isSpeechActive(change) {
+        return (this.focussedMap === '' || change.mapId === this.focussedMap)
+            && MatchLogger.speechSetOn
+            && !this.initNewMatch;
+    }
+
+    static get speechSetOn() {
+        return !!document.querySelector('.audio.on');
+    }
 }
