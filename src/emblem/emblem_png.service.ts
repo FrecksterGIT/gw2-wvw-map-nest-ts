208563f9ecbea88c281b2ca68bd38afb43dd9870
Sven Müller <sven.mueller@breathless-code.de>
2019-08-28 22:28:55 +0200

    chore: dependeny updates

--- src/emblem/emblem_png.service.ts
+++ src/emblem/emblem_png.service.ts
@@ -8,109 +8,113 @@ import IColor from '../gw2api/interfaces/color.interface';
 @Injectable()
 export class EmblemPngService {
 
-  public constructor(private readonly gw2ApiService: Gw2ApiService) {
-  }
-
-  @Cache({cacheType: CacheType.S3Cache})
-  public async getEmblem(guildId): Promise<any> {
-    const guild = await this.gw2ApiService.getGuild(guildId);
-    if (!guild.emblem) {
-      return '';
+    public constructor(private readonly gw2ApiService: Gw2ApiService) {
     }
-    const foreground = await this.getForeground(guild);
-    const background = await this.getBackground(guild);
-    return await this.draw(background, foreground);
-  }
 
-  private async draw(background, foreground) {
-    return new Promise(async (resolve) => {
-      let image: Jimp = await this.drawBackground(background);
-      image = await this.drawForeground(foreground, image);
-      image.getBuffer('image/png', (e, buff) => {
-        resolve(buff);
-      });
-    });
-  }
+    @Cache({cacheType: CacheType.S3Cache})
+    public async getEmblem(guildId): Promise<any> {
+        const guild = await this.gw2ApiService.getGuild(guildId);
+        if (!guild.emblem) {
+            return '';
+        }
+        const foreground = await this.getForeground(guild);
+        const background = await this.getBackground(guild);
+        return await this.draw(background, foreground);
+    }
 
-  private async drawBackground(background): Promise<Jimp> {
-    return new Promise<Jimp>(async (resolve) => {
-      const [images, colors, flipHorizontal, flipVertical]: [string[], IColor[], boolean, boolean] = background;
-      const img = await EmblemPngService.readImageResource(images[0]);
-      img.mirror(flipHorizontal, flipVertical);
-      resolve(EmblemPngService.colorize(img, colors[0]));
-    });
-  }
-
-  private async drawForeground(def, image: Jimp): Promise<Jimp> {
-    return new Promise<Jimp>(async (resolve) => {
-      const [images, colors, flipHorizontal, flipVertical]: [string[], IColor[], boolean, boolean] = def;
-      images.reverse();
-      const img = await EmblemPngService.drawImage(image, images, colors);
-      img.mirror(flipHorizontal, flipVertical);
-      resolve(img);
-    });
-  }
-
-  private static async drawImage(composed: Jimp, images: string[], colors: IColor[], count: number = 0): Promise<Jimp> {
-    return new Promise<Jimp>(async (resolve) => {
-      const image = images.pop();
-      if (image) {
-        const imagePart = await EmblemPngService.readImageResource(image);
-        const color = colors[count === 1 ? 0 : 1];
-        composed.composite(EmblemPngService.colorize(imagePart, color), 0, 0, () => {
-          resolve(this.drawImage(composed, images, colors, ++count));
+    private async draw(background, foreground) {
+        return new Promise(async (resolve) => {
+            let image: Jimp = await this.drawBackground(background);
+            image = await this.drawForeground(foreground, image);
+            image.getBuffer('image/png', (e, buff) => {
+                resolve(buff);
+            });
         });
-      } else {
-        resolve(composed);
-      }
-    });
-  }
+    }
 
-  private static colorize(image: Jimp, color: IColor) {
-    const base = color.cloth;
-    return image.clone().color([
-      {apply: 'greyscale', params: [100]},
-      {apply: 'mix', params: [EmblemPngService.translateColor(base.rgb), 100]}
-    ]);
-  }
+    private async drawBackground(background): Promise<Jimp> {
+        return new Promise<Jimp>(async (resolve) => {
+            const [images, colors, flipHorizontal, flipVertical]: [string[], IColor[], boolean, boolean] = background;
+            const img = await EmblemPngService.readImageResource(images[0]);
+            img.mirror(flipHorizontal, flipVertical);
+            resolve(EmblemPngService.colorize(img, colors[0]));
+        });
+    }
 
-  private static async readImageResource(imgFile) {
-    const PATH = './emblems/';
-    return await Jimp.read(PATH + imgFile);
-  }
+    private async drawForeground(def, image: Jimp): Promise<Jimp> {
+        return new Promise<Jimp>(async (resolve) => {
+            const [images, colors, flipHorizontal, flipVertical]: [string[], IColor[], boolean, boolean] = def;
+            images.reverse();
+            const img = await EmblemPngService.drawImage(image, images, colors);
+            img.mirror(flipHorizontal, flipVertical);
+            resolve(img);
+        });
+    }
 
-  private async getForeground(guild) {
-    const foregrounds = await this.gw2ApiService.getEmblemForegrounds();
-    return this.getConfig(guild, foregrounds, guild.emblem.foreground,
-      'FlipForegroundHorizontal', 'FlipForegroundVertical');
-  }
+    private static async drawImage(composed: Jimp, images: string[], colors: IColor[], count: number = 0): Promise<Jimp> {
+        return new Promise<Jimp>(async (resolve) => {
+            const image = images.pop();
+            if (image) {
+                const imagePart = await EmblemPngService.readImageResource(image);
+                const color = colors[count === 1 ? 0 : 1];
+                composed.composite(EmblemPngService.colorize(imagePart, color), 0, 0, {
+                    mode: Jimp.BLEND_SOURCE_OVER,
+                    opacityDest: 1,
+                    opacitySource: 1
+                }, () => {
+                    resolve(this.drawImage(composed, images, colors, ++count));
+                });
+            } else {
+                resolve(composed);
+            }
+        });
+    }
 
-  private async getBackground(guild) {
-    const backgrounds = await this.gw2ApiService.getEmblemBackgrounds();
-    return this.getConfig(guild, backgrounds, guild.emblem.background,
-      'FlipBackgroundHorizontal', 'FlipBackgroundVertical');
-  }
+    private static colorize(image: Jimp, color: IColor) {
+        const base = color.cloth;
+        return image.clone()
+            .greyscale((err, value) => value.color([
+                {apply: 'mix', params: [EmblemPngService.translateColor(base.rgb), 100]}
+            ]));
+    }
 
-  private async getConfig(guild, res, def, horFlag, verFlag) {
-    const fgDef = res.find((fg) => fg.id === def.id);
-    const files: string[] = fgDef.layers.map((u) => u.substring(u.lastIndexOf('/') + 1, u.length));
+    private static async readImageResource(imgFile) {
+        const PATH = './emblems/';
+        return await Jimp.read(PATH + imgFile);
+    }
 
-    const flags = guild.emblem.flags;
-    const flipHorizontal: boolean = flags.includes(horFlag);
-    const flipVertical: boolean = flags.includes(verFlag);
+    private async getForeground(guild) {
+        const foregrounds = await this.gw2ApiService.getEmblemForegrounds();
+        return this.getConfig(guild, foregrounds, guild.emblem.foreground,
+            'FlipForegroundHorizontal', 'FlipForegroundVertical');
+    }
 
-    const colors = await this.gw2ApiService.getColors();
-    const emblemColors: IColor[] = def.colors.map((color: number) => (colors.find((c: IColor) => c.id === color)));
+    private async getBackground(guild) {
+        const backgrounds = await this.gw2ApiService.getEmblemBackgrounds();
+        return this.getConfig(guild, backgrounds, guild.emblem.background,
+            'FlipBackgroundHorizontal', 'FlipBackgroundVertical');
+    }
 
-    return [files, emblemColors, flipHorizontal, flipVertical];
-  }
+    private async getConfig(guild, res, def, horFlag, verFlag) {
+        const fgDef = res.find((fg) => fg.id === def.id);
+        const files: string[] = fgDef.layers.map((u) => u.substring(u.lastIndexOf('/') + 1, u.length));
 
-  private static translateColor(color) {
-    return '#' + EmblemPngService.pad(color[0]) + EmblemPngService.pad(color[1]) + EmblemPngService.pad(color[2]);
-  }
+        const flags = guild.emblem.flags;
+        const flipHorizontal: boolean = flags.includes(horFlag);
+        const flipVertical: boolean = flags.includes(verFlag);
 
-  private static pad(color: number, width: number = 2, z: string = '0') {
-    const n = color.toString(16);
-    return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
-  }
+        const colors = await this.gw2ApiService.getColors();
+        const emblemColors: IColor[] = def.colors.map((color: number) => (colors.find((c: IColor) => c.id === color)));
+
+        return [files, emblemColors, flipHorizontal, flipVertical];
+    }
+
+    private static translateColor(color) {
+        return '#' + EmblemPngService.pad(color[0]) + EmblemPngService.pad(color[1]) + EmblemPngService.pad(color[2]);
+    }
+
+    private static pad(color: number, width: number = 2, z: string = '0') {
+        const n = color.toString(16);
+        return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+    }
 }
